2a98545364b8181d21e3b17029b9e84fc5d8b5f6
techgeekmorin <168149949+techgeekmorin@users.noreply.github.com>
2025-04-17 18:22:14 -0500

    Initial commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import MainCards from '@/components/MainCards.vue'
+  import MainPagination from '@/components/MainPagination.vue';
+</script>
+
+<template>
+  <MainCards></MainCards>
+  <MainPagination></MainPagination>
+</template>
