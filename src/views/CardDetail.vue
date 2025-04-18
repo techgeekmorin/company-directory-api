7020cda8485cd2682ce94353d845693a545bb775
Roberto Morin <techgeekmorin@gmail.com>
2025-04-17 19:36:10 -0500

    Input form completed

--- src/views/CardDetail.vue
+++ src/views/CardDetail.vue
@@ -23,7 +23,7 @@ const route = useRoute()
         <div v-if="currentEmployee" class="flex flex-col items-center justify-center gap-6">
             <h1 class="text-6xl font-bold p-5">{{ currentEmployee.firstName }} {{ currentEmployee.lastName }}</h1>
             <h1 class="text-3xl p-5">{{ currentEmployee.title }}</h1>
-            <h1 class="text-2xl p-5">{{ currentEmployee.userName }} @southtexascollege.edu</h1>
+            <h1 class="text-2xl p-5">{{ currentEmployee.userName }}@southtexascollege.edu</h1>
             <h1 class="text-2xl p-5">{{ currentEmployee.quote }}</h1>
 
 
